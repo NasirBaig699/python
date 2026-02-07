b24247f98d22c56be0afc311c4d6fc85c53d4739
NasirBaig699 <nb803422@gmail.com>
2026-02-07 18:51:36 +0500

    activityv 4

---
+++ activity4.py
@@ -0,0 +1,11 @@
+#print a sentence
+print("welcome to world of pprogramming")
+
+#print a number
+print(4)
+
+print("hello python ")
+
+print("keywords in pythons are....")
+import keyword
+print(keyword.kwlist)
